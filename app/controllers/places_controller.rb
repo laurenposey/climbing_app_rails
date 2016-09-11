9297b3bea1bde0677714842fbe99a94f2d2c77a2
Lauren Posey <mrslaurenposey@gmail.com>
2016-09-11 14:32:45 -0700

    delete a place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -23,6 +23,26 @@ class PlacesController < ApplicationController
     end
   end
 
+  def edit
+    @place = List.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    if @place.update(place_params)
+      redirect_to places_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    redirect_to places_path
+  end
+
 private
   def place_params
     params.require(:place).permit(:name, :street_address, :city, :state)
