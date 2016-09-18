1e99a799fdb1a465867df535398434633e8a0838
Lauren Posey <mrslaurenposey@gmail.com>
2016-09-17 17:22:41 -0700

    pass spec: the add a place process

--- spec/features/add_place_pages_spec.rb
+++ spec/features/add_place_pages_spec.rb
@@ -5,9 +5,10 @@ describe "the add a place process" do
     visit places_path
     click_link 'New Place'
     fill_in 'Name', :with => 'Planet Granite'
-    fill_in 'Street Address', :with => '1405 NW 14th Ave'
+    fill_in 'Street address', :with => '1405 NW 14th Ave'
     fill_in 'City', :with => 'Portland'
     fill_in 'State', :with => 'Oregon'
+    click_button 'Create Place'
     expect(page).to have_content 'Places'
   end
 end
