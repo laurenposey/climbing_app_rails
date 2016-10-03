ced144896964f60c4255dd5ff8d5cb2fd0d599d0
Lauren Posey <mrslaurenposey@gmail.com>
2016-10-03 13:41:45 -0700

    attempting to call current weather conditions based on zip

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :mailchimp
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :places
 end
---
+++ app/models/weather_condition.rb
@@ -0,0 +1,17 @@
+class Weather_Conditions < ActiveRecord::Base
+
+  private
+
+  def current_weather
+    begin
+      response = RestClient::Request.new(
+      :method => :post,
+      :url => 'http://api.wunderground.com/api/febfd3b059934ab2/conditions/q/=zip.json',
+      :user => ENV['API_KEY'],
+      :payload => { :zip => zip}
+      ).execute
+    rescue
+      throw(:abort)
+    end
+  end
+end
