87ba086ff8b5d942557d4053176b55e9b92c90a9
Lauren Posey <mrslaurenposey@gmail.com>
2016-10-03 15:18:34 -0700

    not working did some edits

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -7,7 +7,7 @@ class Weather < ActiveRecord::Base
     begin
       response = RestClient::Request.new(
       :method => :post,
-      :url => 'http://api.wunderground.com/api/febfd3b059934ab2/conditions/q/' + zip + '.json',
+      :url => 'http://api.wunderground.com/api/8eb4c43ad5/conditions/q/' + zip + '.json',
       :user => ENV['API_KEY'],
       :payload => { :zip => zip,
                     :city => city,
